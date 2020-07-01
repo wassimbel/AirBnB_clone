6d1083217937bea363288c325c9666ba01f33430
wassimbel <wassim.belhedi1@gmail.com>
2020-07-01 13:09:01 -0400

    fix

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """ City Class Unittest """
 import unittest
-from models.city import city
+from models.city import City
 
 
 class Test_City(unittest.TestCase):
     """ City class testing """
+    def setUp(self):
+        """setup"""
+        self.user = City()
+
 
     def test_city(self):
         """ test """
@@ -15,6 +19,32 @@ class Test_City(unittest.TestCase):
         self.assertTrue(hasattr(c, "created_at"))
         self.assertTrue(hasattr(c, "updated_at"))
 
+    def test_str(self):
+        """ test print """
+        str = "[{}] ({}) {}".format("City", self.user.id,
+                                    self.user.__dict__)
+        self.assertEqual(print(str), print(self.user))
+
+    def test_to_dict(self):
+        """to_dict testing"""
+        dic = City()
+        self.assertIsInstance(dic.to_dict(), dict)
+
+    def test_to_dict(self):
+        """Test dict"""
+        self.assertIsInstance(self.user.to_dict(), dict)
+
+    def test_id(self):
+        """ id testing """
+        u = City()
+        u1 = City()
+        self.assertNotEqual(u.id, u1.id)
+
+    def test_doc_1(self):
+        """ test """
+        test = " subclass City "
+        test1 = City.__doc__
+        self.assertEqual(test, test1)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,10 +2,14 @@
 """ State Class Unittest """
 import unittest
 from models.state import State
-
+from models.base_model import BaseModel
 
 class Test_State(unittest.TestCase):
     """ State class testing """
+
+    def setUp(self):
+        """setup"""
+        self.S = State()
 
     def test_state(self):
         """ test """
@@ -14,6 +18,33 @@ class Test_State(unittest.TestCase):
         self.assertTrue(hasattr(s, "id"))
         self.assertTrue(hasattr(s, "created_at"))
         self.assertTrue(hasattr(s, "updated_at"))
+
+    def test_str(self):
+        """ test print """
+        str = "[{}] ({}) {}".format("State", self.S.id,
+                                    self.S.__dict__)
+        self.assertEqual(print(str), print(self.S))
+
+    def test_to_dict(self):
+        """to_dict testing"""
+        dic = State()
+        self.assertIsInstance(dic.to_dict(), dict)
+
+    def test_to_dict(self):
+        """Test dict"""
+        self.assertIsInstance(self.S.to_dict(), dict)
+
+    def test_id(self):
+        """ id testing """
+        s = State()
+        s1 = State()
+        self.assertNotEqual(s.id, s1.id)
+
+    def test_doc_1(self):
+        """ test """
+        test = "  subclass State "
+        test1 = State.__doc__
+        self.assertEqual(test, test1)
 
 
 if __name__ == '__main__':
