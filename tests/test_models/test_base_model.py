3e5ca6e43bc7fd9ceebdb6189cd82a0d0108232a
wassimbel <wassim.belhedi1@gmail.com>
2020-07-01 08:36:21 -0400

    fix

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -25,12 +25,9 @@ class TestBaseModel(unittest.TestCase):
         b = BaseModel()
         b.name = "Holberton"
         b.my_number = 89
-        b.id = 1
         self.assertEqual(b.name, "Holberton")
         self.assertEqual(b.my_number, 89)
-        self.assertEqual(b.id, 1)
         self.assertEqual(b.id, b.id)
-        self.assertNotEqual(b.id, self.base.id)
 
     def test_kwargs(self, *args, **kwargs):
         """ testing args and kwargs """
