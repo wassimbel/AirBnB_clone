7565d3248d6e2e521b40b2b0fcd5881566d799b4
wassimbel <wassim.belhedi1@gmail.com>
2020-07-01 08:57:18 -0400

    fix

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -21,18 +21,8 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(hasattr(self.base, "id"))
         self.assertTrue(hasattr(self.base, "created_at"))
         self.assertTrue(hasattr(self.base, "updated_at"))
-        b = BaseModel()
-        b.name = "Holberton"
-        b.my_number = 89
-        self.assertEqual(b.name, "Holberton")
-        self.assertEqual(b.my_number, 89)
         self.assertTrue(isinstance(self.base.created_at, datetime))
-        self.assertNotEqual(self.base.id, b.id)
 
-    def test_test1(self):
-        test1 = "id"
-        test = BaseModel()
-        self.assertIsNotNone(test.id, test1)
 
     def test_kwargs(self, *args, **kwargs):
         """ testing args and kwargs """
