acae64b0a15e228bd605164ffdab8ac64649532a
wassimbel <wassim.belhedi1@gmail.com>
2020-07-01 09:46:54 -0400

    f

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,9 +2,9 @@
 """Unittest for class FileStorage """
 
 import unittest
+import os
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-import os
 
 
 class TestFileStorage(unittest.TestCase):
@@ -18,3 +18,23 @@ class TestFileStorage(unittest.TestCase):
         f = FileStorage()
         f.new(b)
         self.assertEqual(b, f.all()["BaseModel" + "." + b.id])
+
+    def test_a(self):
+        """ testing  """
+        b = BaseModel()
+        f = FileStorage()
+        f.new(b)
+        f.save()
+        test = f.all()
+        f.reload()
+        self.assertEqual(test, f.all())
+
+    def test_file(self):
+        """ testing file_path """
+        test = FileStorage()
+        self.assertIsInstance(test._FileStorage__file_path, str)
+        self.assertTrue(os.path.exists("file.json"))
+
+
+if __name__ == '__main__':
+    unittest.main()
